721705e63539cbacc65e19471633d23858f10d3f
Greg Curtis <curtisgr@gmail.com>
2016-06-14 16:30:39 -0400

    missing function

--- addon/index.js
+++ addon/index.js
@@ -1,3 +1,10 @@
+function nearestPeriodicValue (point, value, period) {
+  // Adjust our value by an amount given by the closest #
+  // of periods contained in the distance between the point
+  // and the value
+  return value - period * Math.round((value - point) / period);
+};
+  
 function containedPeriodicValues (start, end, value, period) {
   // Inclusive start; exclusive end
   if (start === end) {
