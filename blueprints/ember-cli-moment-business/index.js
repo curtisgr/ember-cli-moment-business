faa4fc6ad36e5a622f0c57715fa71652996a819f
Greg Curtis <curtisgr@gmail.com>
2016-06-14 14:12:37 -0400

    I don't know what I'm doing

--- blueprints/ember-cli-moment-business/index.js
+++ blueprints/ember-cli-moment-business/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
 module.exports = {
-  normalizeEntityName: function() {
-    // this prevents an error when the entityName is
-    // not specified (since that doesn't actually matter
-    // to us
-  },
+  description: ''
+
+  normalizeEntityName: function() {},
 
   afterInstall: function() {
     return this.addBowerPackageToProject('moment-business');
